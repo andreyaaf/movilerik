3450204d73c32115801ecf603d7a6cd6f659f100
andreyaaf <148802452+andreyaaf@users.noreply.github.com>
2024-08-20 10:22:03 -0500

    Add files via upload

---
+++ aora_adso/app/(tabs)/boomkmark.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Boomkmark = () => {
+  return (
+    <View>
+      <Text>Boomkmark</Text>
+    </View>
+  )
+}
+
+export default Boomkmark
---
+++ aora_adso/app/index.tsx
@@ -0,0 +1,19 @@
+import {Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className ="text-3xl font-pblack" > Aora!</Text>
+      <StatusBar style="auto"/>
+      <Link href="/home" style={{color:'blue'}}>
+      go to home </Link>
+      
+      </View>
+  
+)
+}
+
